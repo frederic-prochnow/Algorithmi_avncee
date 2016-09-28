86c0169baf2ac01906994bba201b71eedaa5b2b5
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-09-28 18:04:21 +0200

    test complet

--- ALGO-AVAN--SEMAINE-4--TP-4/test/ExoSetTest.java
+++ ALGO-AVAN--SEMAINE-4--TP-4/test/ExoSetTest.java
@@ -19,4 +19,30 @@ public class ExoSetTest {
 		res.add(20);  res.add(30);
 		assertEquals(res,ExoSet.inter(ens1,ens2));
 	}
+	
+	@Test
+	public void testUnion(){
+		Set<Integer> ens1 = new HashSet<Integer>();
+		Set<Integer> ens2 = new HashSet<Integer>();
+		Set<Integer> res = new HashSet<Integer>();
+		ens1.add(10); ens1.add(20);
+		ens1.add(30); ens1.add(40);
+		ens2.add(20); ens2.add(30);
+		ens2.add(50); ens2.add(60);
+		res.addAll(ens1);
+		res.addAll(ens2);
+		assertEquals(res,ExoSet.union(ens1,ens2));
+	}
+	
+	@Test
+	public void testIsIn(){
+		Set<Integer> ens1 = new HashSet<Integer>();
+		Set<Integer> ens2 = new HashSet<Integer>();
+		ens1.add(10); ens1.add(20);
+		ens1.add(30); ens1.add(40);
+		ens2.add(20); ens2.add(30);
+		ens2.add(50); ens2.add(60);
+		ens2.add(10); ens2.add(40);
+		assertTrue(ExoSet.isIn(ens1,ens2));
+	}
 }

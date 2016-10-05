a3558e0aa8d219f83e1cd715e93647401141d594
Frederic PROCHNOW <frederic.prochnow@etudiant.univ-lille1.fr>
2016-10-05 17:57:22 +0200

    tp6

--- ALGO-AVAN--SEMAINE-5--TP-6/src/Adressage.java
+++ ALGO-AVAN--SEMAINE-5--TP-6/src/Adressage.java
@@ -1,4 +1,36 @@
 
-public class Adressage {
+public class Adressage implements HashTable{
+
+	MaListe[] mListe;
+	
+	@Override
+	public int put(String key, int value) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int get(String key) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean remove(String key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(String key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
